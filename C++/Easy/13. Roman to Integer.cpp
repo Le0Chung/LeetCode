2f2357f792927cfd1d55148f92af0c7d2141b480
Le0Chung <40647004s@gapps.ntnu.edu.tw>
2022-05-18 17:57:00 +0800

    test: 嘗試使用Regex(極慢)

--- C++/Easy/13. Roman to Integer.cpp
+++ C++/Easy/13. Roman to Integer.cpp
@@ -1,36 +1,14 @@
 class Solution {
-private:
-    map<char, int> Roman_table;
 public:
     Solution() {
-        // 建立table
-        Roman_table['I'] = 1;
-        Roman_table['V'] = 5;
-        Roman_table['X'] = 10;
-        Roman_table['L'] = 50;
-        Roman_table['C'] = 100;
-        Roman_table['D'] = 500;
-        Roman_table['M'] = 1000;
     }
     ~Solution() {
-        Roman_table.clear();
     }
     int romanToInt(string s) {
-        // result 紀錄最終結果
-        int result = 0;
-        for(int i = 0; i < s.length() - 1; i++) {
-            // 前 >= 後 => 將前者數值加入結果
-            if(Roman_table[s[i]] >= Roman_table[s[i + 1]]) {
-                result += Roman_table[s[i]];
-            }
-            // 前 < 後 => 將結果扣除前者數值
-            else {
-                result -= Roman_table[s[i]];
-            }
-        }
-        // 最後不用比對直接加入結果
-        result += Roman_table[s.back()];
+        regex reg("(M)?(M)?(M)?(CM)?(CD)?(D)?(C)?(C)?(C)?(XC)?(XL)?(L)?(X)?(X)?(X)?(IX)?(IV)?(V)?(I)?(I)?(I)?");
+        smatch sm;
+        regex_match(s, sm, reg);
         // 回傳結果
-        return result;
+        return (sm[1].length() ? 1000 : 0) + (sm[2].length() ? 1000 : 0) + (sm[3].length() ? 1000 : 0) + (sm[4].length() ? 900 : 0) + (sm[5].length() ? 400 : 0) + (sm[6].length() ? 500 : 0) + (sm[7].length() ? 100 : 0) + (sm[8].length() ? 100 : 0) + (sm[9].length() ? 100 : 0) + (sm[10].length() ? 90 : 0) + (sm[11].length() ? 40 : 0) + (sm[12].length() ? 50 : 0) + (sm[13].length() ? 10 : 0) + (sm[14].length() ? 10 : 0) + (sm[15].length() ? 10 : 0) + (sm[16].length() ? 9 : 0) + (sm[17].length() ? 4 : 0) + (sm[18].length() ? 5 : 0) + (sm[19].length() ? 1 : 0) + (sm[20].length() ? 1 : 0) + (sm[21].length() ? 1 : 0);
     }
 };
