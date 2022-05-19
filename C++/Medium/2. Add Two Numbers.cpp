da4add4cd569a307a6aa5586fbce756088c76276
Le0Chung <40647004s@gapps.ntnu.edu.tw>
2022-05-19 14:35:15 +0800

    improvement: 使用linklist特性

--- C++/Medium/2. Add Two Numbers.cpp
+++ C++/Medium/2. Add Two Numbers.cpp
@@ -9,42 +9,61 @@
  * };
  */
 class Solution {
+private:
+    void carryToLastNode(ListNode* node) {
+        // 避免傳入空指標，所以先傳入不為空的ListNode
+        if (node->next == nullptr) {
+            node->next = new ListNode(0);
+        }
+        // 處理後再指到下個node
+        node = node->next;
+        const int carry = 1;
+        const int ct = node->val + carry;
+        node->val = ct % 10;
+        // 需要進位就繼續call function
+        if (ct == 10) {
+            carryToLastNode(node);
+        }
+        return;
+    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // 創造新的Linklist，並記錄第一個節點
-        ListNode* nl = new ListNode(0);
-        ListNode* first = nl;
+        // 以l1為主
+        ListNode* first = l1;
         // 紀錄總和與進位
         int ct = 0;
         int carry = 0;
-        while(l1 != nullptr || l2 != nullptr) {
-            if (l1 == nullptr) {
-                ct = l2->val + carry;
-                l2 = l2->next;
-            }
-            else if (l2 == nullptr) {
-                ct = l1->val + carry;
-                l1 = l1->next;
-            }
-            else {
-                ct = l1->val + l2->val + carry;
-                l1 = l1->next;
-                l2 = l2->next;
-            }
-            nl->next = new ListNode(ct % 10);
-            nl = nl->next;
+        while(true) {
+            ct = l1->val + l2->val + carry;
             if (ct >= 10) {
+                ct -= 10;
                 carry = 1;
             }
             else {
                 carry = 0;
             }
-        }
-        // 檢查是否要進位到最後一個節點
-        if(carry == 1) {
-            nl->next = new ListNode(1);
+            l1->val = ct;
+            if (l1->next == nullptr) {
+                // l1為空就把l2接上來並判斷是否繼續進位
+                l1->next = l2->next;
+                if (carry == 1) {
+                    carryToLastNode(l1);
+                }
+                break;
+            }
+            else if (l2->next == nullptr) {
+                // l2為空就判斷是否繼續進位
+                if (carry == 1) {
+                    carryToLastNode(l1);
+                }
+                break;
+            }
+            else {
+                l1 = l1->next;
+                l2 = l2->next;
+            }
         }
         // 回傳Linklist開頭
-        return first->next;
+        return first;
     }
 };
